46dfbd4ac593ead080325f3a666b71aa9beca0d6
impervguin <leon53000@yandex.ru>
2022-04-24 12:13:34 +0300

    created register form

---
+++ forms/user.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    password_confirmation = PasswordField("password confirmation", validators=[DataRequired()])
+    submit = SubmitField("register")
