a13b23eb01b24f7904ff2768d6e66a6849ece502
impervguin <leon53000@yandex.ru>
2022-04-24 20:13:04 +0300

    created db parser

---
+++ physical_values_db_transfer.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def transfer():
+    data = {}
+    db = sqlite3.connect("db/physical_values.db")
+    cur = db.cursor()
+    tables_information = cur.execute("SELECT * FROM sqlite_master").fetchall()
+    tables = {}
+    for table_inf in tables_information:
+        parsed_inf = table_inf[4].split()
+        col_names = [parsed_inf[i] for i in range(4, len(parsed_inf) - 1, 2)]
+        tables[table_inf[1]] = col_names
+    for table in tables:
+        data[table] = []
+        for elems in cur.execute(f"SElECT * FROM {table}").fetchall():
+            row = {}
+            for i in range(len(tables[table])):
+                row[tables[table][i]] = elems[i]
+            data[table].append(row)
+    return data
