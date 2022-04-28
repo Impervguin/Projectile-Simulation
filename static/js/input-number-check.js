135569a3a05e206e8f6ea12f0dccafb1573547cf
impervguin <leon53000@yandex.ru>
2022-04-28 22:42:12 +0300

    updated script for restricting input values

--- static/js/input-number-check.js
+++ static/js/input-number-check.js
@@ -4,6 +4,19 @@ function check_number(input) {
     if (point_cnt > 1) {
         pos = ch.indexOf('.');
         ch = ch.slice(0, pos) + ch.slice(pos + 1, ch.length);
-    }
+    };
+
+    if ((ch.charAt(ch.length - 1) != "0" || (point_cnt == 0)) && (ch.charAt(ch.length - 1) != ".") && (ch.length != 0)) {
+        max = parseFloat(input.getAttribute('data-max'));
+        min = parseFloat(input.getAttribute('data-min'));
+        ch = parseFloat(ch);
+        if (max < ch) {
+            ch = max;
+        } else if (min > ch) {
+            ch = min;
+        };
+    };
+
+
     input.value = ch;
 };
